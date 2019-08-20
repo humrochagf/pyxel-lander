be096e5bca5d6e77efc70382506b5e5863ee9127
Humberto Rocha <humrochagf@gmail.com>
2019-08-20 08:28:22 -0300

    Fix classifier

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         'Environment :: X11 Applications',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Operating System :: OS Independent'
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
